f491a26c5e2268099058b9251a860dd94ed3fb78
John Doe <johndoe@example.com>
2023-09-20 12:17:45 +0200

    okokokokokoko

---
+++ lib/score.dart
@@ -0,0 +1,12 @@
+class Score {
+  int playerScore = 0;
+  int opponentScore = 0;
+
+  void incrementPlayerScore() {
+    playerScore++;
+  }
+
+  void incrementOpponentScore() {
+    opponentScore++;
+  }
+}
